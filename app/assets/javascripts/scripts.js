d1c7f325f8ac7dd5453ff5e53e70b58429c6869c
Scott Norvell <scottlnorvell@gmail.com>
2013-10-24 20:00:21 -0400

    skeleton version

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,30 +1,6 @@
 
-var oembed_obj, song;
 
 $(function() {
-  SC.initialize({
-    client_id: '560d601638096e37de666da699486214'
-  })
-
-  $('#widget-button').on('click', function() {
-    var url = $('#widget-track-url').val();
-
-    SC.oEmbed(url, {color: "ff6600", show_comments: false},  function(oembed) { 
-      console.log("oEmbed response: ", oembed); 
-      oembed_obj = oembed;
-      // $('#widget').html(oembed.html);
-    });
-  });
-
-  $('#stream-button').on('click', function() {
-    var url = $('#stream-track-url').val();
-    SC.get('/resolve', {url: url }, function(track) {
-      SC.stream('/tracks/' + track.id, function(sound) {
-        song = sound;
-      })
-    });
-
-  });
-
+  
 
 });
